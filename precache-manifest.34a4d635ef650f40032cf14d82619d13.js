8b5ec6932986c348bcbd3ca75c25a6bb31ac6462
unknown <michaelarayamurcia@gmail.com>
2020-08-23 07:54:27 -0600

    Updates

---
+++ precache-manifest.34a4d635ef650f40032cf14d82619d13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "454fc9099b10000f3a65d482aa02dbe0",
+    "url": "/Robolex2/index.html"
+  },
+  {
+    "revision": "fd2e55e6efb65e831c04",
+    "url": "/Robolex2/static/css/main.31133030.chunk.css"
+  },
+  {
+    "revision": "efdaa8f6541fe00250cb",
+    "url": "/Robolex2/static/js/2.dbc8e97e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robolex2/static/js/2.dbc8e97e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd2e55e6efb65e831c04",
+    "url": "/Robolex2/static/js/main.5e14abe6.chunk.js"
+  },
+  {
+    "revision": "fd0ec40253ae326b0f26",
+    "url": "/Robolex2/static/js/runtime-main.97fc9ee8.js"
+  }
+]);
